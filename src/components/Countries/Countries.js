8226b7a31fafd6403ce0501b528a4e14ec415cb4
Koushik Ahmad <koushikahmad123@gmail.com>
2023-02-01 00:53:39 +0600

    added countries

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+import './countries.css';
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+
+    }, []);
+
+    return (
+        <div>
+            <h1>Hi!! Hello!! from Countires: {countries.length}</h1>
+            <div className='countries-container'>
+                {
+                    countries.map(country => <Country
+                        country={country}
+                        key={country.cca3}
+                    ></Country>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
